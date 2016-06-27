6bc785f2c3a4b2d580d9893475dedb1c30ada23d
Eli Murray <emurray@tampabay.com>
2016-06-27 13:04:40 -0400

    took out unused tasks

--- fabfile/local.py
+++ fabfile/local.py
@@ -2,17 +2,6 @@ from datetime import datetime
 
 from fabric.api import task, lcd, local, get
 
-
-@task
-def dbBackup(append=None):
-    filename = datetime.now().strftime("%Y-%m-%d")
-    if (append):
-        filename += "-" + append
-    with lcd("data"):
-        local(
-            "pg_dump -h 54.211.200.87 -U newproducts -W -F c -f %s.dbdump survey" % filename
-        )
-
 @task
 def getBackup():
     filename = datetime.now().strftime("%Y-%m-%d")
